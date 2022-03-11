0d3aad22c8f54298e6e22ef60363c6b8822c9a07
Prakhar Baskotra <33579380+theprakharb@users.noreply.github.com>
2022-03-11 16:35:04 +0530

    Arrays in Golang

---
+++ arrays.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// q := []int{1, 2, 3, 4}
+	// fmt.Println(len(q), cap(q))
+
+	a := make([]int, 5)
+	// b := make([]int, 0, 10) //* Len = 0 and Cap = 10
+	fmt.Println(a)
+	// fmt.Println(b)
+
+	// a = append(a, 5)
+	// fmt.Println(len(a))
+
+	//* Range function use
+	// str_arr := []string{"a", "b", "c", "d", "e"}
+	// for i, c := range str_arr {
+	// 	fmt.Println(c, " is on index ", i)
+}
